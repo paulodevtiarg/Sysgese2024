d399e6ac056091e072f32dd7d837e266813999d6
paulodevtiarg <prn.dev.ti@gmail.com>
2024-11-29 09:39:38 -0300

    atualizando

--- SysGeSeApp2024/obj/Release/net8.0/SysGeSeApp2024.AssemblyInfo.cs
+++ SysGeSeApp2024/obj/Release/net8.0/SysGeSeApp2024.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysGeSeApp2024")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+968847ab7b09baa058011a3ed6a26109f71a60d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysGeSeApp2024")]
